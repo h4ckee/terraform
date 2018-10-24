ad8394bfe56dfb464b7f971fc65fec20bbde23cd
kpect <kpect@protonmail.com>
2018-10-24 11:59:46 +0300

    update

--- main.tf
+++ main.tf
@@ -2,10 +2,12 @@ provider "aws" {
   region = "eu-central-1"
 }
 
-resource "aws_instance" "example" {
-  ami                    = "ami-0bdf93799014acdc4"
-  instance_type          = "t2.micro"
-  vpc_security_group_ids = ["${aws_security_group.instance.id}"]
+data "aws_availability_zones" "all" {}
+
+resource "aws_launch_configuration" "cluster" {
+  image_id        = "ami-0bdf93799014acdc4"
+  instance_type   = "t2.micro"
+  security_groups = ["${aws_security_group.cluster-sg.id}"]
 
   user_data = <<-EOF
 	#!/bin/bash
@@ -13,13 +15,13 @@ resource "aws_instance" "example" {
 	nohup busybox httpd -f -p "${var.server_port}" &
 	EOF
 
-  tags {
-    Name = "terraform-example"
+  lifecycle {
+    create_before_destroy = true
   }
 }
 
-resource "aws_security_group" "instance" {
-  name = "terraform-example-instance"
+resource "aws_security_group" "cluster-sg" {
+  name = "cluster-sg"
 
   ingress {
     from_port   = "${var.server_port}"
@@ -27,8 +29,76 @@ resource "aws_security_group" "instance" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  lifecycle {
+    create_before_destroy = true
+  }
 }
 
-output "public_ip" {
-  value = "${aws_instance.example.public_ip}"
+resource "aws_security_group" "elb-sg" {
+  name = "elb-sg"
+
+  ingress {
+    from_port   = "${var.lb_ingress_port}"
+    to_port     = "${var.lb_ingress_port}"
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = "${var.lb_egress_port}"
+    to_port     = "${var.lb_egress_port}"
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_elb" "cluster-elb" {
+  name               = "cluster-elb"
+  availability_zones = ["${data.aws_availability_zones.all.names}"]
+  security_groups    = ["${aws_security_group.elb-sg.id}"]
+
+  listener {
+    lb_port           = "${var.lb_ingress_port}"
+    lb_protocol       = "http"
+    instance_port     = "${var.server_port}"
+    instance_protocol = "http"
+  }
+
+  health_check {
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    timeout             = 3
+    interval            = 30
+    target              = "HTTP:${var.server_port}/"
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_autoscaling_group" "cluster-asg" {
+  launch_configuration = "${aws_launch_configuration.cluster.id}"
+  availability_zones   = ["${data.aws_availability_zones.all.names}"]
+
+  load_balancers    = ["${aws_elb.cluster-elb.name}"]
+  health_check_type = "ELB"
+
+  min_size = 2
+  max_size = 10
+
+  tag {
+    key                 = "Name"
+    value               = "cluster-asg"
+    propagate_at_launch = true
+  }
+}
+
+output "elb_dns_name" {
+  value = "${aws_elb.cluster-elb.dns_name}"
 }
--- variables.tf
+++ variables.tf
@@ -2,3 +2,13 @@ variable "server_port" {
   description = "The port the server will use for HTTP requests"
   default     = 8080
 }
+
+variable "lb_ingress_port" {
+  description = "The port the load balancer will use for HTTP requests"
+  default     = 80
+}
+
+variable "lb_egress_port" {
+  description = "The port the load balancer will use for health checks"
+  default     = 0
+}
